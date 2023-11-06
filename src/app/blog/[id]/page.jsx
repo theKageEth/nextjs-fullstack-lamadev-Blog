1fc6148836147cda3b500e9c764e2c4fcc8768d1
thekage.eth <m.siddik56@hotmail.com>
2023-11-06 15:46:51 +0000

    vercel url5

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -3,7 +3,7 @@ import { notFound } from "next/navigation";
 
 async function getData(id) {
   const api = process.env.NEXT_PUBLIC_VERCEL_URL;
-  const res = await fetch(`${api}/api/posts/${id}`, {
+  const res = await fetch(`https://${api}/api/posts/${id}`, {
     cache: "no-store",
   });
 
